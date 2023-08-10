29c07b90751e82bb3199fc84cf58c87828ed3857
Bongsug Chae <bchae@users.campus>
2023-08-09 19:28:37 -0500

    first commit

--- app.py
+++ app.py
@@ -1,31 +1,90 @@
 # Credits: 
-# https://datasciencechalktalk.com/2019/10/22/building-machine-learning-apps-with-streamlit/
-# https://towardsdatascience.com/streamlit-101-an-in-depth-introduction-fc8aad9492f2
+# https://towardsdatascience.com/deploying-ml-models-using-streamlit-5d6212453bdd
+# https://github.com/NateDiR/diamond_price_streamlit_prototype_app/blob/main/streamlit.py
 
+import xgboost as xgb
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
-# Classifiers
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+#Loading up the Regression model we created
+model = xgb.XGBRegressor()
+model.load_model('xgb_model.json')
 
-#for validating your classification model
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import roc_auc_score
+#Caching the model for faster loading
+@st.cache
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/bonchae/data/master/heartattack_train.csv') 
+# Define the prediction function
+def predict(carat, cut, color, clarity, depth, table, x, y, z):
+    #Predicting the price of the carat
+    if cut == 'Fair':
+        cut = 0
+    elif cut == 'Good':
+        cut = 1
+    elif cut == 'Very Good':
+        cut = 2
+    elif cut == 'Premium':
+        cut = 3
+    elif cut == 'Ideal':
+        cut = 4
+
+    if color == 'J':
+        color = 0
+    elif color == 'I':
+        color = 1
+    elif color == 'H':
+        color = 2
+    elif color == 'G':
+        color = 3
+    elif color == 'F':
+        color = 4
+    elif color == 'E':
+        color = 5
+    elif color == 'D':
+        color = 6
+    
+    if clarity == 'I1':
+        clarity = 0
+    elif clarity == 'SI2':
+        clarity = 1
+    elif clarity == 'SI1':
+        clarity = 2
+    elif clarity == 'VS2':
+        clarity = 3
+    elif clarity == 'VS1':
+        clarity = 4
+    elif clarity == 'VVS2':
+        clarity = 5
+    elif clarity == 'VVS1':
+        clarity = 6
+    elif clarity == 'IF':
+        clarity = 7
+    
+
+    prediction = model.predict(pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]], columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']))
+    return prediction
 
 
+st.title('Diamond Price Predictor')
+st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.header('Enter the characteristics of the diamond:')
+carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
+cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
+color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
+clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
+depth = st.number_input('Diamond Depth Percentage:', min_value=0.1, max_value=100.0, value=1.0)
+table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
+x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
+
+if st.button('Predict Price'):
+    price = predict(carat, cut, color, clarity, depth, table, x, y, z)
+    st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
+
+
+st.markdown("## Party time!")
+st.write("Yay! You're done with this tutorial of Streamlit. Click below to celebrate.")
+btn = st.button("Celebrate!")
+if btn:
+    st.balloons()
